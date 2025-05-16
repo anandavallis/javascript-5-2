37aa8d708abafad3467e8d2f2753889fdddebe56
anandavallis <sanandavalli7@gmail.com>
2025-05-16 14:56:04 +0530

    Create concept2.js

---
+++ concept2.js
@@ -0,0 +1,14 @@
+
+    <h2>Array includes()</h2>
+    
+
+        function arrayIncludes(array, value){
+            return array.includes(value);
+        }
+        const fruits = ['Apple', 'Banana', 'Cherry'];
+        const searchValue = 'Banana';
+
+        const result = arrayIncludes(fruits,searchValue);
+        console.log(result);
+        
+    
