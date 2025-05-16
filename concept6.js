cc91e9c01d68b7ca55407df0501529def6f620cc
anandavallis <sanandavalli7@gmail.com>
2025-05-16 15:01:19 +0530

    Create concept6.js

---
+++ concept6.js
@@ -0,0 +1,15 @@
+
+    <h2> Destructuring Assignment</h2>
+    
+        function personDetails(person){
+            const {name,age, city } = person;
+        
+        console.log(name);
+        console.log(age);
+        console.log(city);
+        };
+        personDetails({ name: 'Alice', age: 25, city: 'New York' });
+
+        
+        
+   
