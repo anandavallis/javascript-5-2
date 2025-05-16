eddb5e21162c3e6434eba1488d8b9d2a74ac0f1d
anandavallis <sanandavalli7@gmail.com>
2025-05-16 15:02:16 +0530

    Create concept8.js

---
+++ concept8.js
@@ -0,0 +1,9 @@
+
+    <h2>Rest Parameters</h2>
+    
+       function sum(...numbers){
+        return numbers.reduce((total , num) => total + num, 0);
+       }
+       console.log(sum(1, 2, 3, 4, 5));
+       
+   
