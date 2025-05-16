304b0638ddd9ff77389ca7953b6eb4831868012b
anandavallis <sanandavalli7@gmail.com>
2025-05-16 15:00:45 +0530

    Create concept5.js

---
+++ concept5.js
@@ -0,0 +1,10 @@
+
+    <h2>Template Litrals</h2>
+    
+        function introducePerson(name,age){
+             return `Hello my name is ${name} and I am ${age} years old.`;
+
+        }
+        console.log(introducePerson('Jonh',30));
+        
+    
